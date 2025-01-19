11fe582f58451d474e40970095440f980e4bd031
user-of-name <belte.francis@gmail.com>
2025-01-20 01:05:03 +0200

    fucked up which template goes where

--- Art_Projects/file_not_found.php
+++ Art_Projects/file_not_found.php
@@ -1,3 +1,3 @@
 <?php
 $project_id = 12;
-include "../includes/basic_layout_img_priority.php";
+include "../includes/basic_layout_split.php";
--- Art_Projects/foraging_in_the_netherlands.php
+++ Art_Projects/foraging_in_the_netherlands.php
@@ -1,3 +1,3 @@
 <?php
 $project_id = 4;
-include "../includes/basic_layout_img_priority.php";
+include "../includes/basic_layout_split.php";
