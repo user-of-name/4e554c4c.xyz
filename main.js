15182c6b7ef12ac11feffd8cd339d9106a3a323e
robin <robindehaandesign@gmail.com>
2025-01-21 23:28:02 +0100

    pop-up max-height on 500px

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+  document.documentElement.style.setProperty('--vh', `${viewportHeight * 0.01}px`)
+})
+ 
  const mobileIndexToggler = document.getElementById("indexToggler");
  const mobileIndexContent = document.getElementById('indexContent');
 
