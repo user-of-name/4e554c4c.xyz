5b2c7de41a7991e0d0a011e6029cae95869f3a8a
robin <robindehaandesign@gmail.com>
2025-07-31 21:09:14 +0200

    bottom line fixed

--- includes/basic_layout_img_priority.php
+++ includes/basic_layout_img_priority.php
@@ -25,9 +25,11 @@ include "../includes/content_db.php";
 
     <!--  main body of the page begins -->
     <div class="container-fluid">
+    <div class="fixed-bottom start-0 w-100 p0 bg-black border-top border-primary z-1" style="height:25px">
+    </div>     
         <div class="row">
             <!-- shuffled classes around so they properly show mobile version -->
-            <div class="col-lg-3 col-md-6 mobile-d overflow-y-auto" style="max-height: 90vh;">
+            <div class="col-lg-3 col-md-6 mobile-d overflow-y-auto" style="max-height: 92vh;">
                  <div id="mobileD" class="container-fluid flex-grow-1 p-0">
                    <div class="text-center text-uppercase text-primary">
                         <a class="title" onclick="text_toggle()">
@@ -44,15 +46,15 @@ include "../includes/content_db.php";
                 
             </div>
 
-            <div class="col-lg-9 col-md-6 mobile-d-content order-first order-md-last overflow-y-auto p-2" style="max-height: 90vh;">
-                <div class="row d-flex overflow-y-auto p-2">  
+            <div class="col-lg-9 col-md-6 mobile-d-content order-first order-md-last overflow-y-auto">
+                <div class="row d-flex overflow-y-auto p-2" style="max-height: 92vh;">  
                     <!-- added bootstrap attributes to trigger modal -->
                     <!-- data-bs-slide-to="0" handles on which slide carousel opens, currently not working as it needs to be attached to specific trigger -->             
                 <?php include "../includes/get_project_images.php";?>
                 </div>
-            </div>            
+            </div>       
         </div>
-            </div>
+     </div>
 <!-- modal trial -->
 <!-- Modal -->
 <?php include "../includes/image_carousel_modal.php"; ?>
