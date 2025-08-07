300f965a4b1b0fed29bcf4cc41d581c34ed91b05
user-of-name <belte.francis@gmail.com>
2025-08-07 17:21:30 +0300

    3d descr

--- Documentary_Work/101.php
+++ Documentary_Work/101.php
@@ -1,3 +1,4 @@
 <?php
 $project_id = 14;
+$description = "A 3D Archive of the Anarchist Library and the UFF Studio. Scanned before its eviction in 2024.";
 include "../includes/basic_layout_split.php";
